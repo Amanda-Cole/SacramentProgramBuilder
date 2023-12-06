755f525fde594a89a7a44ca71ef4045e229afad6
DANIEL GERDES <ger19007@byui.edu>
2023-12-05 20:56:52 -0800

    added more validation

--- Models/SacramentProgram.cs
+++ Models/SacramentProgram.cs
@@ -13,49 +13,72 @@ namespace SacramentProgramBuilder.Models
 
         [Display(Name = "Presiding")]
         [Required]
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? PresidingName { get; set; }
 
         [Display(Name ="Conducting")]
         [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? ConductingName { get; set; }
 
         [Display(Name ="Invocation")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? OpeningPrayerName { get; set; }
 
         [Display(Name ="Opening Hymn")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? OpeningHymnName { get; set; }
+
         [Display(Name = "Page")]
+        [Required]
+        [Range(1,341)]
         public int? OpeningHymnNum { get; set; }
 
         [Display(Name = "Sacrament Hymn")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? SacramentHymnName { get; set; }
 
         [Display(Name = "Page")]
+        [Required]
+        [Range(1, 341)]
         public int? SacramentHymnNum { get; set; }
 
         [Display(Name = "Speaker Type")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? SpeakerType { get; set; } //Youth speaker, Speaker, or Article of Faith Enum?
 
         [Display(Name = "Speaker")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? SpeakerName { get; set; }
 
         [Display(Name = "Special Musical Number")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? SpecialMusicalNumber { get; set; }//choir Name or musical quest name
 
         [Display(Name = "Intermediate Hymn")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? IntermediateHymnName { get; set; }
 
         [Display(Name = "Page")]
+        [Range(1, 341)]
         public int? IntermediateHymnNum { get; set; }
 
         [Display(Name = "Closing Prayer")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? ClosingPrayerName { get; set; }
 
         [Display(Name = "Closing Hymn")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? ClosingHymnName { get; set; }
 
         [Display(Name = "Page")]
+        [Required]
+        [Range(1, 341)]
         public int? ClosingHymnNum { get; set; }
 
     }
