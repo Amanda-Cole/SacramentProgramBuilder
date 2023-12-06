e36b6a4d933947924e27ce7c07f661487cb153a1
Kendra Bryant <kbryant@efilecabinet.com>
2023-12-05 22:11:46 -0700

    Updated validation

--- Models/Speaker.cs
+++ Models/Speaker.cs
@@ -7,9 +7,11 @@ namespace SacramentProgramBuilder.Models
         public int Id { get; set; }
 
         [Display(Name = "Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Name { get; set; }
 
         [Display(Name = "Speaker Type")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string? SpeakerType { get; set; } //Youth speaker, Speaker, or Article of Faith Enum?
     }
 }
